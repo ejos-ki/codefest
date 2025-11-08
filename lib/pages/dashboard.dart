5a5e803782694105f5289ae13218efcfbe3dc39f
Jeo Latorre <latorrejeo1@gmail.com>
2025-11-08 20:48:55 +0800

    modified sidebar

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -16,29 +16,40 @@ class _DashboardPageState extends State<DashboardPage> {
       backgroundColor: Colors.blueGrey[50],
       body: SafeArea(
         child: Stack(
-          
           children: [
-            SingleChildScrollView(
-            child: Column(children: [
-              CustomHeader(),
+            // SingleChildScrollView(
+            //   child: Column(children: [
+            //     CustomHeader(),
 
-              SizedBox(height: 80,),
-            ],),
-          ),
+            //     SizedBox(height: 80,),
+            //   ],),
+            // ),
 
-          Positioned (
-            top: 80,
-            left: 10,
-            bottom: -10,
-            width: 350,
-            child: Container( 
-              color: Colors.blue,
-              width: 50,
-              height: double.infinity,
-              child: Column (
+            Padding(
+              padding: EdgeInsets.zero,
+              child: Container(
+                width: 350,
+                height: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.brown[300],
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(30),
+                    bottomRight: Radius.circular(30),
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    SizedBox(height: 20,),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 20,),
+                      child: Text("ErqauKer", style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white, letterSpacing: 2),)
+                    )
+                  ],
+                ),
               )
-            )
-          ),
+            ),
+
+
           ],
         )
       ),
